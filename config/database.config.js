a2923b44143545d361a3a96cc76d5bb8f734a030
franko4don <franko4don@yahoo.com>
2018-09-08 23:00:12 +0100

    changed mongodb login

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/easy-notes'
-    url: 'mongodb://franko4don:chuky4don@ds251002.mlab.com:51002/demo'
+    url: 'mongodb://markogbuify:marken09128@ds251022.mlab.com:51022/markdemodb'
 }
