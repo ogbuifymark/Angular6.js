1e32139da814abff8482f37bd54aa7f6c9994c15
franko4don <franko4don@yahoo.com>
2018-09-08 22:37:15 +0100

    changed mongodb url

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    // url: 'mongodb://localhost:27017/easy-notes'
+    url: 'mongodb://franko4don:chuky4don@ds251002.mlab.com:51002/demo'
 }
