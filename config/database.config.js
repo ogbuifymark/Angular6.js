e38bc46c87661289d54d8a13edc76b072f5c1c25
Ogbuify Markanthony <ogbuifymark@gmail.com>
2018-09-14 00:29:34 +0100

    saved

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://markogbuify:marken09128@ds251022.mlab.com:51022/markdemodb'
+    // url: 'mongodb://markogbuify:marken09128@ds251022.mlab.com:51022/markdemodb'
+    url: 'mongodb://localhost:27017/bezop_chart'
 }
